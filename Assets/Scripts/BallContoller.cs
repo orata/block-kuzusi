59431338e30d16fef7dd642967f0a23f6a344d0c
unknown <yuma107@outlook.jp>
2015-06-06 15:55:39 +0900

    初めてのコミット

---
+++ Assets/Scripts/BallContoller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallContoller : MonoBehaviour {
+	public int power = 500;
+		private Rigidbody _rigidbody;
+
+	void Awake(){
+		_rigidbody = this.GetComponent<Rigidbody> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		this._rigidbody.AddForce (Vector3.right * power);
+		this._rigidbody.AddForce (Vector3.forward * power);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/RacketController1.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RacketController1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			if (Input.GetKey ("right")) {
+			transform.position += new Vector3 (1f, 0f, 0f); 
+
+		}
+
+	}
+}
