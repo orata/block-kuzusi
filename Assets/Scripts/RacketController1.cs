c9f1ced3ab473c7e372d8edf681f6a1da9b39f47
unknown <yuma107@outlook.jp>
2015-06-06 16:27:09 +0900

    ｂｃ

--- Assets/Scripts/RacketController1.cs
+++ Assets/Scripts/RacketController1.cs
@@ -11,7 +11,7 @@ public class RacketController1 : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 			if (Input.GetKey ("right")) {
-			transform.position += new Vector3 (1f, 0f, 0f); 
+			transform.position += new Vector3 (Input.GetAxis("Horizontal"), 0f, 0f); 
 
 		}
 
